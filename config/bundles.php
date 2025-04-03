0c6c97d445277ff0d7a3cde28bb54072c07d5d1d
michalkurecka <michal.kurecka@superkoders.com>
2025-04-03 20:16:17 +0200

    vite init

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
 ];
