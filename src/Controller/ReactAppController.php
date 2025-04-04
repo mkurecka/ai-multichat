58ae4dd1272885b84133cc668f89764eb29de0ac
michalkurecka <michal.kurecka@superkoders.com>
2025-04-04 17:09:01 +0200

    callback fix

--- src/Controller/ReactAppController.php
+++ src/Controller/ReactAppController.php
@@ -9,6 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ReactAppController extends AbstractController
 {
     #[Route('/app/{reactRouting}', name: 'app_react', defaults: ['reactRouting' => null], requirements: ['reactRouting' => '^(?!api).+'])]
+    #[Route('/app/callback', name: 'app_callback')]
     public function index(): Response
     {
         return $this->render('react_app/index.html.twig');
